fcecc799e7ee814477b1354937463874d90065cc
Jean Pool Gutierrez <jeanpool.dev@hotmail.com>
2026-02-01 10:36:02 -0500

    Pipes Globales - A nivel de Aplicacion

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -8,14 +8,13 @@ import {
   ParseUUIDPipe,
   Patch,
   Post,
-  UsePipes,
-  ValidationPipe,
 } from '@nestjs/common';
 import { CarsService } from './cars.service';
 import type { Car } from './interfaces/car.interface';
 import { CreateCarDto } from './dto/create-car.dto';
 
 @Controller('cars')
+// @UsePipes(ValidationPipe)
 export class CarsController {
   constructor(private readonly carsService: CarsService) {}
 
@@ -30,7 +29,6 @@ export class CarsController {
   }
 
   @Post()
-  @UsePipes(ValidationPipe)
   createCar(@Body() createCarDto: CreateCarDto) {
     return createCarDto;
   }
--- src/cars/dto/create-car.dto.ts
+++ src/cars/dto/create-car.dto.ts
@@ -1,9 +1,10 @@
-import { IsString } from 'class-validator';
+import { IsString, MinLength } from 'class-validator';
 
 export class CreateCarDto {
   @IsString({ message: 'Brand must be a string ' })
   readonly brand: string;
 
   @IsString()
+  @MinLength(3, { message: 'Model must be at least 3 characters long' })
   readonly model: string;
 }
