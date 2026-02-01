bc19d6771920144f03adb60fbe1d04b5a7fd44de
Jean Pool Gutierrez <jeanpool.dev@hotmail.com>
2026-01-31 19:21:00 -0500

    Post, Patch y Delete

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,5 +1,20 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { CarsService } from './cars.service';
+
+export class CreateCarDto {
+  brand: string;
+  model: string;
+  year: number;
+}
 
 @Controller('cars')
 export class CarsController {
@@ -13,5 +28,23 @@ export class CarsController {
   @Get(':id')
   getCarById(@Param('id', ParseIntPipe) id: number) {
     return this.carsService.findOneById(id);
+  }
+
+  @Post()
+  createCar(@Body() body: CreateCarDto) {
+    return body;
+  }
+
+  @Patch(':id')
+  updateCar(@Param('id', ParseIntPipe) id: number, @Body() body: CreateCarDto) {
+    return body;
+  }
+
+  @Delete(':id')
+  deleteCar(@Param('id', ParseIntPipe) id: number) {
+    return {
+      method: 'delete',
+      id,
+    };
   }
 }
